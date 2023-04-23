19a383367c9976344ce069fe1770fff8cc9af31c
unknown <julijana_sk@hotmail.com>
2023-04-23 03:12:28 +0200

    filtering done

--- main.js
+++ main.js
@@ -1,3 +1,78 @@
+// filtering cards
+
+document.querySelector("#filter-marketing").addEventListener("change", filterMarketing);
+document.querySelector("#filter-coding").addEventListener("change", filterCoding);
+document.querySelector("#filter-design").addEventListener("change", filterDesign);
+
+
+function filterMarketing() {
+    hideAllCards();
+
+    if(document.querySelector("#filter-marketing").checked) {
+        const marketingCards = document.querySelectorAll(".cards-marketing");
+        marketingCards.forEach(marketingCards => {
+            marketingCards.style.display = "inline-block";
+        });
+        
+        document.querySelector("#filter-coding").checked = false;
+        document.querySelector("#filter-design").checked = false;
+    } else {
+        showAllCards();
+    }
+}
+
+
+function filterCoding() {
+    hideAllCards();
+
+    if(document.querySelector("#filter-coding").checked) {
+        const codingCards = document.querySelectorAll(".cards-coding");
+        codingCards.forEach(codingCards => {
+            codingCards.style.display = "inline-block";
+        });
+        document.querySelector("#filter-marketing").checked = false;
+        document.querySelector("#filter-design").checked = false;
+    } else {
+        showAllCards();
+    }
+}
+
+function filterDesign() {
+    hideAllCards();
+
+    if(document.querySelector("#filter-design").checked) {
+        const designCards = document.querySelectorAll(".cards-design");
+        designCards.forEach(designCards => {
+            designCards.style.display = "inline-block";
+        });
+        document.querySelector("#filter-coding").checked = false;
+        document.querySelector("#filter-marketing").checked = false;
+    } else {
+        showAllCards();
+    }
+}
+
+
+function hideAllCards() {
+    const allCards = document.querySelectorAll(".card");
+
+    allCards.forEach(card => {
+        card.style.display = "none";
+    });
+}
+
+function showAllCards() {
+    const allCards = document.querySelectorAll(".card");
+
+    allCards.forEach(card => {
+        card.style.display = "inline-block";
+    });
+}
+// filtering cards - end
+
+
+// validating data in form
+
 const form = document.getElementById('form');
 const username = document.getElementById('username');
 const email = document.getElementById('email');
@@ -81,3 +156,6 @@ const validateInputs = () => {
     }
     
 };
+// validating data - end 
+
+
