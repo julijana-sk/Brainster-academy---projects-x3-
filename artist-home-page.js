8fd4a4aa5c5ddd2479fb733273ae832685ba90b2
unknown <julijana_sk@hotmail.com>
2023-08-07 00:52:05 +0200

    cards

---
+++ artist-home-page.js
@@ -0,0 +1,47 @@
+// function showAllCards() {
+//     const allCards = document.querySelectorAll(".card");
+
+//     allCards.forEach(card => {
+//         card.style.display = "inline-block";
+//     });
+// }
+// filtering cards - end
+
+// navbar responsive
+// function openNav() {
+//     document.getElementById("myNav").style.width = "100%";
+//     }
+
+//     function closeNav() {
+//     document.getElementById("myNav").style.width = "0%";
+//     }
+
+// navbar responsive - end
+
+function myFunction() {
+  const x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+// filter layer display
+// const filterBtnBasic = document.getElementById("filter-button-basic");
+// const filterBtnChecked = document.getElementById("filter-button-checked");
+
+// function openNav() {
+//     document.getElementById("filter-layer").style.width = "100%";
+//     filterBtnBasic.style.display = "none";
+//     filterBtnChecked.style.display = "flex";
+//     filterBtnChecked.style.justifyContent = "end";
+
+        
+// }
+
+// function closeNav() {
+//     document.getElementById("filter-layer").style.width = "0%";
+//     filterBtnBasic.style.display = "flex";
+//     filterBtnChecked.style.display = "none";
+// }
